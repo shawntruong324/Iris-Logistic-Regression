8bb23f5d66e152b80b70e94888d24788dfa5b24a
shawntruong324 <shawn.truong@outlook.com>
2024-09-25 12:58:24 -0400

    grad descent smearing
    Smearing error may have been caused due to temp in grad_descent function being defined as a normal array, while the outputs of the assignments appended to temp are in the form of np array's dtype=np.float64 
    
    Not tested yet. 
    
    X classes are divided by # of training examples and features.
    rows x columns = features x num training examples (5 x 50)
    Theta is initialized as 
    rows x columns = binary classification model * parameters  (3 x 5)
    Thus theta x X_class is the appropriate matrix multiplication method, providing an matrix of 1x50 hypothesis outputs 
    The format should be in the form of M x 1, where M is the number of training examples but this does suffice for now.

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -3,6 +3,7 @@ import numpy as np
 # This program takes the Iris database from UC Irvine's Machine Learning Repository and does a logistic regression for classification of the species based on the four features below:
 # sepal length, sepal width, petal length, and petal width with the expected output of species. The four features are continuous with units of centimeters (cm)
 # The following program will utlize a one-vs-all classification strategy, the sigmoid function, and assumes the features are normally distributed. 
+# The cost function utilized is the mean squared error. A stochastic gradient descent approach will be used.  
 # Two classes are linearly inseperable and 1 class is linearly separable (as per the repository).
 # Learning rate of gradient descent is experimentally found.
 
@@ -34,6 +35,7 @@ X_class1 = [[], [], [], [], []]
 X_class2 = [[], [], [], [], []]
 X_class3 = [[], [], [], [], []]
 
+#Separate the features by class (species). They are provided in batches of 50, so they can be separated by batches of 50. 
 def separate_class(X):
     for k in range(50):
         X_class1[0].append(0)
@@ -47,6 +49,7 @@ def separate_class(X):
                 X_class2[i+1].append(X[i][j])
             else:
                 X_class3[i+1].append(X[i][j])
+    X_class1, X_class2, X_class3 = np.array(X_class1, dtype=np.float64), np.array(X_class2, dtype=np.float64), np.array(X_class3, dtype=np.float64)
 
 separate_class(X)
 
@@ -64,7 +67,10 @@ def cost_function_derivative(theta, X, Y, j):
     cost = cost/50
     return cost
 
+# Smearing error appears in this function where for initial theta = 0 and X = X_class1 + X_class2, 
+# the outputs are provided as 0. or - 0.
 def grad_descent(theta, X, Y, alpha):
-    temp = []
+    temp = np.zeros((1, 50), dtype=np.float64), 
     for j in range(len(theta)):
-        temp.append(theta[i] - alpha * cost_function_derivative(theta, X, Y, j))
+        temp[j] = (theta[i] - alpha * cost_function_derivative(theta, X, Y, j))
+    return temp 
